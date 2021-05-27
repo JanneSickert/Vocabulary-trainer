05fd51580d8d7bbf0859d98a893b9c9fbbe1307b
JanneSickert <janne.sickert@gmx.de>
2021-05-27 20:43:56 +0200

    Finish add new vocabulary

--- NewVocabulary.py
+++ NewVocabulary.py
@@ -3,13 +3,33 @@ import tkinter as tk
 app = tk.Tk()
 app.geometry("1300x1500")
 
+MESSAGE = "Save new Vocabulary"
+PATH = "database.js"
+
+
+def make_js(mystr):
+    CODE = ("var cdb = [", "]\n", "var DATA = cdb")
+    i = 0
+    strGoFile = CODE[0]
+    arr = mystr.split("\n")
+    while i < (len(arr) - 5):
+        strGoFile = strGoFile + "[\"" + arr[i] + "\", \"" + arr[i + 1] + "\"], "
+        i = i + 3
+    strGoFile = strGoFile + "[\"" + arr[len(arr) - 3] + "\", \"" + arr[len(arr) - 2] + "\"]" + CODE[1] + CODE[2]
+    return strGoFile
+
+
+def write_in_js(mystr):
+    PATH = "database.js"
+    file = open(PATH, "w")
+    file.write(mystr)
+    file.close()
+
 
 def button_action():
-    # entry_text = textAria.get()
-    print("Button was pressed")
+    entry_text = textAria.get("1.0", "end")
+    write_in_js(make_js(entry_text))
 
-
-MESSAGE = "Save new Vocabulary"
 
 buttonExample1 = tk.Button(app,
                            command=button_action,
